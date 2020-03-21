06e6bfc8840e370ddfb47db6950a6a25300cec33
Masayuki Ono <mono0926@gmail.com>
2020-03-21 15:29:22 +0900

    Comment out platform == TargetPlatform.macOS temporarily

--- lib/src/extensions/theme_data.dart
+++ lib/src/extensions/theme_data.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 extension ThemeDataEx on ThemeData {
+  // ignore: lines_longer_than_80_chars
+  // TODO(mono): Add platform == TargetPlatform.macOS when it is supported on stable
   bool get isCupertinoStyle =>
-      platform == TargetPlatform.iOS || platform == TargetPlatform.macOS;
+      platform == TargetPlatform.iOS; // || platform == TargetPlatform.macOS;
 }
