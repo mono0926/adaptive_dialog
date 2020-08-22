300810a0e6aa80dea1f53c4717196c7b784d9beb
Masayuki Ono <mono0926@gmail.com>
2020-08-22 12:56:44 +0900

    Update packages

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Router;
 import 'package:mono_kit/mono_kit.dart';
 import 'package:provider/provider.dart';
 
