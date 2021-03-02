7ca70fa25990a7d796d9cec512141e6b4e78b0d6
Masayuki Ono <mono0926@gmail.com>
2021-03-02 15:27:52 +0900

    Update packges

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import mono_kit
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  MonoKitPlugin.register(with: registry.registrar(forPlugin: "MonoKitPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
