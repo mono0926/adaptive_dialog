cef528e16ccfb652d6f2ef272d8633c7a6c4b3aa
Masayuki Ono <mono0926@gmail.com>
2022-04-04 13:56:56 +0900

    Add _StyleDropdownButton

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:example/pages/home_page.dart';
 import 'package:flutter/material.dart' hide Router;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,10 +13,13 @@ class App extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(routerProvider);
+    final platform = ref.watch(
+      adaptiveStyleNotifier.select((s) => s.platform),
+    );
     return MaterialApp.router(
       title: title,
-      theme: lightTheme(),
-      darkTheme: darkTheme(),
+      theme: lightTheme().copyWith(platform: platform),
+      darkTheme: darkTheme().copyWith(platform: platform),
       routeInformationParser: router.routeInformationParser,
       routerDelegate: router.routerDelegate,
       localizationsDelegates: const [
--- example/lib/pages/home_page.dart
+++ example/lib/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:example/app.dart';
 import 'package:example/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:recase/recase.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +14,9 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(App.title),
+        actions: const [
+          _StyleDropdownButton(),
+        ],
       ),
       body: ListView(
         children: [
@@ -25,5 +31,47 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class _StyleDropdownButton extends ConsumerWidget {
+  const _StyleDropdownButton({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: DropdownButton<AdaptiveStyle>(
+          value: ref.watch(adaptiveStyleNotifier),
+          items: AdaptiveStyle.values
+              .map(
+                (style) => DropdownMenuItem(
+                  value: style,
+                  child: Text(
+                    style.name.pascalCase,
+                  ),
+                ),
+              )
+              .toList(),
+          onChanged: (style) =>
+              ref.read(adaptiveStyleNotifier.notifier).update((_) => style!),
+        ),
+      ),
+    );
+  }
+}
+
+final adaptiveStyleNotifier = StateProvider((ref) => AdaptiveStyle.adaptive);
+
+extension AdaptiveStyleX on AdaptiveStyle {
+  TargetPlatform? get platform {
+    switch (this) {
+      case AdaptiveStyle.material:
+        return TargetPlatform.android;
+      case AdaptiveStyle.cupertino:
+        return TargetPlatform.iOS;
+      case AdaptiveStyle.adaptive:
+        return null;
+    }
   }
 }
