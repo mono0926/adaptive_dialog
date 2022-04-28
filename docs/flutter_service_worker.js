6ec96e0abd29958968db1a706fea124ecb881d7e
mono <mono0926@gmail.com>
2022-04-28 20:24:19 +0900

    Update docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "ff966ab969ba381b900e61629bfb9789",
-"index.html": "1094a85957d2700bfb05bb745f732cc9",
-"/": "1094a85957d2700bfb05bb745f732cc9",
-"main.dart.js": "80d29c7d0b76a476b7ff6ba9f69304cc",
+"index.html": "2075c4da6e4b649f7c4a780feae19359",
+"/": "2075c4da6e4b649f7c4a780feae19359",
+"main.dart.js": "a7cfaaafae3f3732b538334e8622c8f0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
