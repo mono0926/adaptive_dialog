7c3c93a86260e13d0c993c480fb9f9406871301b
mono <mono0926@gmail.com>
2022-04-29 07:32:45 +0900

    Make draggalbe MacosDraggableDialog

--- lib/src/helper/macos_draggable_dialog.dart
+++ lib/src/helper/macos_draggable_dialog.dart
@@ -15,6 +15,7 @@ class MacosDraggableDialog extends StatefulWidget {
 }
 
 class _MacosDraggableDialogState extends State<MacosDraggableDialog> {
+  var _offset = Offset.zero;
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -28,48 +29,56 @@ class _MacosDraggableDialogState extends State<MacosDraggableDialog> {
       Colors.white.withOpacity(0.45),
       Colors.white.withOpacity(0.15),
     );
-    return Dialog(
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: borderRadius,
-      ),
-      backgroundColor: color,
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            width: 2,
-            color: innerBorderColor,
-          ),
+    return Transform.translate(
+      offset: _offset,
+      child: Dialog(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
           borderRadius: borderRadius,
         ),
-        foregroundDecoration: BoxDecoration(
-          border: Border.all(
-            color: brightness.resolve(
-              Colors.black.withOpacity(0.23),
-              Colors.black.withOpacity(0.76),
+        backgroundColor: color,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: innerBorderColor,
             ),
+            borderRadius: borderRadius,
           ),
-          borderRadius: borderRadius,
-        ),
-        child: SizedBox(
-          width: 500,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                height: 28,
-                decoration: BoxDecoration(
-                  color: Color.lerp(color, Colors.white, 0.12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      blurRadius: 1,
-                    ),
-                  ],
-                ),
+          foregroundDecoration: BoxDecoration(
+            border: Border.all(
+              color: brightness.resolve(
+                Colors.black.withOpacity(0.23),
+                Colors.black.withOpacity(0.76),
               ),
-              widget.child,
-            ],
+            ),
+            borderRadius: borderRadius,
+          ),
+          child: SizedBox(
+            width: 500,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onPanUpdate: (details) => setState(() {
+                    _offset += details.delta;
+                  }),
+                  child: Container(
+                    height: 28,
+                    decoration: BoxDecoration(
+                      color: Color.lerp(color, Colors.white, 0.12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 1,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                widget.child,
+              ],
+            ),
           ),
         ),
       ),
