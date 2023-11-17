04a22be8f0b25ef286fa3ff6c3d35e541a4ced93
mono <mono0926@gmail.com>
2023-11-17 12:49:48 +0900

    Migrate to TextScaler
    - https://docs.flutter.dev/release/breaking-changes/deprecate-textscalefactor

--- lib/src/modal_action_sheet/cupertino_modal_action_sheet.dart
+++ lib/src/modal_action_sheet/cupertino_modal_action_sheet.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:adaptive_dialog/src/action_callback.dart';
 import 'package:adaptive_dialog/src/extensions/extensions.dart';
 import 'package:flutter/cupertino.dart';
@@ -29,19 +27,13 @@ class CupertinoModalActionSheet<T> extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final mediaQuery = MediaQuery.of(context);
     final title = this.title;
     final message = this.message;
     return PopScope(
       canPop: canPop,
       onPopInvoked: onPopInvoked,
-      child: MediaQuery(
-        data: mediaQuery.copyWith(
-          // `CupertinoAlertDialog` overrides textScaleFactor
-          // to keep larger than 1, but `CupertinoActionSheet` doesn't.
-          // https://twitter.com/_mono/status/1266997626693509126
-          textScaler: TextScaler.linear(max(1, mediaQuery.textScaleFactor)),
-        ),
+      child: MediaQuery.withClampedTextScaling(
+        minScaleFactor: 1,
         child: CupertinoActionSheet(
           title: title == null ? null : Text(title),
           message: message == null ? null : Text(message),
