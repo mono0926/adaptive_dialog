95adccff8312d8d8bee7afe9a35778339b354466
mono0926 <1255062+mono0926@users.noreply.github.com>
2026-09-09 04:06:37 +0000

    chore: auto deploy web [skip ci]

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2846272140" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1841644942" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
